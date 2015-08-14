a7b9dad6443f9d2f2637b4360b605c871c689652
Joshua Smith <kernelsmith@kernelsmith.com>
2015-08-14 17:25:35 -0500

    adds example plugin

---
+++ example/example.plugin.zsh
@@ -0,0 +1,7 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
