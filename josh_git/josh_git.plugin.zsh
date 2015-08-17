92b3246406b63b6a4eafbc62ec00510988410ee0
Joshua Smith <kernelsmith@kernelsmith.com>
2015-08-17 11:41:19 -0500

    move git-obliterate to josh-git

--- josh_git/josh_git.plugin.zsh
+++ josh_git/josh_git.plugin.zsh
@@ -34,3 +34,22 @@ up() {
     [ "$stash" != "No local changes to save" ] && git stash pop
   done
 }; #export -f up
+
+git-obliterate() {
+  if (( $# == 0 ));then
+    echo "usage: git-obliterate file1 [file2...]"
+  fi
+  files=$@
+  for file in $files; do
+    git filter-branch -f --index-filter "git rm -r --cached $file --ignore-unmatch" \
+      --prune-empty --tag-name-filter cat -- --all
+    #git ignore $file
+    echo "$file" >> .gitignore
+    git add .gitignore
+    git commit -m "Add $file to .gitignore"
+  done
+  echo "You're new .gitignore is:"
+  cat .gitignore
+  echo
+  echo "Now, if you really mean it, do a: git push --force"
+}
