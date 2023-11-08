cd77195506eaf26367d97ebf1f7125db6ffa9787
kernelsmith <kernelsmith kernelsmith com>
2023-11-08 14:00:40 -0600

    don't need the exports on a zsh plugin

--- utils/utils.plugin.zsh
+++ utils/utils.plugin.zsh
@@ -3,8 +3,6 @@
 #
 # NOTE:  add the function to extras.rc if it's rarely used
 
-debug "utils.rc"
-
 # mkdir if the dir doesn't already exist
 chk_mkdir() {
   # make a directory (with -p) if it doesn't exist
@@ -13,20 +11,20 @@ chk_mkdir() {
     mkdir -p $1 || type -t die && die "Can't create directory...aborting" $_ERR_CANT_WRITE_DIR
   fi
 }
-export -f chk_mkdir
+#export -f chk_mkdir
 
 # get a nicely formatted, 'ls'-sortable timestamp
 # does not automatically add a \n at the end
 stampit() {
   echo -n "$@.$(date +%Y%m%d-%H%M%S)"
 }
-export -f stampit
+#export -f stampit
 
 # easily change the terminal title
 ttitle() {
   echo -e '\033k'$@'\033\'
 }
-export -f ttitle
+#export -f ttitle
 
 rgrep() {
   if [ -n "${2}" ]; then
@@ -35,9 +33,10 @@ rgrep() {
     find -L . -type f -name \*.*rb -exec grep -n -i -H --color "$1" {} \;
   fi
 }
-export -f rgrep
+#export -f rgrep
 
 # find large files.  $1 is min size in megs to show 
 find_large() {
   find ~/ -size +${1}M -ls
 }
+#export -f find_large
